7773875aebc4f2789eb7ab1c299c67d7f26c3a5a
twenty2industries <burak.bostanli.1@web.de>
2025-06-11 20:55:13 +0200

    feat: navbar components created. implemented @landingPageComponents

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from './landingPage/landingPage.component';
+import { NavbarComponent } from './landingPage/navbar/navbar.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, LandingPageComponent],
+  imports: [CommonModule, RouterOutlet, LandingPageComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/landingPage/landingPage.component.ts
+++ src/app/landingPage/landingPage.component.ts
@@ -1,13 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+
 
 @Component({
   selector: 'app-landingPage',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NavbarComponent],
   template: `
   <section>
+    <app-navbar></app-navbar>
     <h1 class="fontRaleway">SAKURA RAMEN</h1>
     <h2>BEST RAMEN IN TOWN</h2>
   </section>
